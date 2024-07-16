7c8ef9e2baecdaf95caa17cde5cb3bef80bc0ee6
TheTechnikfreak <paulschmid511+spam@gmail.com>
2024-07-16 19:55:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+var resultContainer = document.getElementById('qr-reader-results');
+var lastResult, countResults = 0;
+
+function onScanSuccess(decodedText, decodedResult) {
+    if (decodedText !== lastResult) {
+        ++countResults;
+        lastResult = decodedText;
+        resultsDiv.innerHTML 
+        // Handle on success condition with the decoded message.
+        console.log(`Scan result ${decodedText}`, decodedResult);
+    }
+}
+
+var html5QrcodeScanner = new Html5QrcodeScanner(
+    "qr-reader", { fps: 10, qrbox: 250 });
+html5QrcodeScanner.render(onScanSuccess);
 async function searchProduct() {
   const input = document.getElementById("productInput").value;
   const resultsDiv = document.getElementById("results");
